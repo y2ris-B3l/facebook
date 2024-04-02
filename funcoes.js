a9f2231a80a7d72016c91b6b6e718d1ef2e0480d
y2ris-B3l <165829506+y2ris-B3l@users.noreply.github.com>
2024-04-02 16:19:07 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+linkParaFotos = document.getElementById("linkParaFotos");
+linkParaFotos.innerHTML = `Veja fotos de ${user.name}`;
+linkParaVideos = document.getElementById("linkParaVideos");
+linkParaVideos.innerHTML = `Veja videos de ${user.name}`;
+linkParaMensagens = document.getElementById("linkParaMensagens");
+linkParaMensagens.innerHTML = `Mande uma Mensagem para ${user.name}`;
+linkParaCutucar = document.getElementById("linkParaCutucar");
+linkParaCutucar.innerHTML = `Cutuque ${user.name}`;
+
